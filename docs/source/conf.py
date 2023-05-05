310d4a980e620712aff35083c86b6302e6260854
root <root@hrothgar3.hec.lancs.ac.uk>
2023-05-05 13:37:40 +0100

    basic launch page

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,9 +19,9 @@ sys.path.insert(0, os.path.abspath('../'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'probflow'
-copyright = '2019, Brendan Hasz'
-author = 'Brendan Hasz'
+project = 'hec-3.0'
+copyright = '2023, Mike Pacey'
+author = 'Mike Pacey'
 
 # The short X.Y version
 version = '2.0'
@@ -69,7 +69,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = en
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
